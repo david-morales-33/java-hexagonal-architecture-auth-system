6fef87fe1068fe3187160bee61b42cd2877489ba
david-morales-33 <davidmorales33.97@gmail.com>
2025-02-11 17:00:35 -0500

    ADD: file <META-INF>

--- src/main/java/com/dmx/server/Main.java
+++ src/main/java/com/dmx/server/Main.java
@@ -5,10 +5,9 @@ import com.dmx.server.controllers.UserController;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Holaaaa");
 
         UserController controller = new UserController();
-        controller.create(
+        controller.management(
                 "833eafb5-5432-4e5d-a113-32e2aff91ed9",
                 "Luisa Fernanda Gonsalez",
                 "Luisa123.4@gmail.com",
